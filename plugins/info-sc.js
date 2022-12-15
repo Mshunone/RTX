216c5da667bdf5a9c592c3b72056f99aa1950237
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-12-15 23:03:37 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let ye = `@${m.sender.split`@`[0]}`
 let esce = `
-Hai ${ye} Bot Ini Menggunakan Script :\n• https://github.com/BOTCAHX/RTXZY-MD
+Hai ${ye} Lagi cari sc ya chek sini :\n• https://bit.ly/SaxiaShop
 `
 conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
 }
