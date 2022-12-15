1d76549889d814eeab96795fb94d80e34e33a9ae
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-12-15 21:47:20 +0700

    Update _anti+212.js

--- plugins/_anti+212.js
+++ plugins/_anti+212.js
@@ -5,6 +5,18 @@ handler.before = async function (m) {
    	global.db.data.users[m.sender].banned = true
    }
    
+    if (m.sender.startsWith('91' || '91')) {
+   	global.db.data.users[m.sender].banned = true
+   }
+   
+    if (m.sender.startsWith('92' || '92')) {
+   	global.db.data.users[m.sender].banned = true
+   }
+   
+    if (m.sender.startsWith('90' || '90')) {
+   	global.db.data.users[m.sender].banned = true
+   }
+   
    if (m.sender.startsWith('265' || '265')) {
    	global.db.data.users[m.sender].banned = true
    } 
